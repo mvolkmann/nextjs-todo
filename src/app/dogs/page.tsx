d68b7a89bbf3a5264bc27470979e3c4103bf6f82
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-08-13 15:11:55 -0500

    progress

--- src/app/dogs/page.tsx
+++ src/app/dogs/page.tsx
@@ -2,7 +2,10 @@ import { getDogs, type Dog } from '@/lib/dogs-api';
 import DogComponent from './DogComponent';
 
 // This shows one way to preventing caching API results used by this page.
-export const revalidate = 0; // number, not boolean
+// A value of zero causes "dynamic rendering"
+// which does not use cached responses.
+// A value of false is the default which does use cached responses.
+export const revalidate = 0;
 
 export default async function DogsPage() {
   const dogs: Dog[] = await getDogs();
